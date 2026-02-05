0c3036d2ab70c81edf2998da18107e9abe3bac57
Juliana Cano <iamojules@gmail.com>
2026-02-05 18:36:59 -0500

    Create config.php
    Add production database configuration using environment variables

---
+++ config/config.php
@@ -0,0 +1,14 @@
+<?php
+$host = getenv('MYSQLHOST');
+$port = getenv('MYSQLPORT');
+$dbname = getenv('MYSQLDATABASE');
+$user = getenv('MYSQLUSER');
+$pass = getenv('MYSQLPASSWORD');
+
+try {
+    $conn = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4", $user, $pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    die("Database connection failed: " . $e->getMessage());
+}
+?>
